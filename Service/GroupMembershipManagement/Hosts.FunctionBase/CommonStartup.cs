b1313f65a539f00a4b65ac85751263c5af348fea
Alberto Rios <alrios@microsoft.com>
2021-01-27 09:44:21 -0800

    Updated nuget packages

--- Service/GroupMembershipManagement/Hosts.FunctionBase/CommonStartup.cs
+++ Service/GroupMembershipManagement/Hosts.FunctionBase/CommonStartup.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
