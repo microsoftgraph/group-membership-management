28a1ec1bf3f9cfa073f04da2dec5492abaf4ab7d
Alberto Rios <alrios@microsoft.com>
2021-02-22 15:57:46 -0800

    Added AzureTableBackup function

--- Service/GroupMembershipManagement/Repositories.Contracts/ILoggingRepository.cs
+++ Service/GroupMembershipManagement/Repositories.Contracts/ILoggingRepository.cs
@@ -2,13 +2,14 @@
 // Licensed under the MIT license.
 using Entities;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Repositories.Contracts
 {
     public interface ILoggingRepository
-    {        
+    {
         public Dictionary<string, string> SyncJobProperties { get; set; }
-        Task LogMessageAsync(LogMessage logMessage);
+        Task LogMessageAsync(LogMessage logMessage, [CallerMemberName] string caller = "", [CallerFilePath] string file = "");
     }
 }
