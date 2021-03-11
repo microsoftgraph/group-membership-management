250de45a18363cf7fe6973e586992bcfee7ea4ae
Alberto Rios <alrios@microsoft.com>
2021-03-11 15:31:22 -0800

    Use scoped logging and jobrepository

--- Service/GroupMembershipManagement/Hosts/GraphUpdater/Function/Startup.cs
+++ Service/GroupMembershipManagement/Hosts/GraphUpdater/Function/Startup.cs
@@ -11,8 +11,10 @@ using Microsoft.Extensions.Options;
 using Microsoft.Graph;
 using Repositories.Contracts;
 using Repositories.GraphGroups;
+using Repositories.Logging;
 using Repositories.MembershipDifference;
 using Repositories.SyncJobsRepository;
+using System.Linq;
 
 // see https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
 [assembly: FunctionsStartup(typeof(Hosts.GraphUpdater.Startup))]
@@ -27,25 +29,29 @@ namespace Hosts.GraphUpdater
         {
             base.Configure(builder);
 
+            var logger = builder.Services.Where(s => s.ServiceType == typeof(ILoggingRepository) && s.Lifetime == ServiceLifetime.Singleton).First();
+            builder.Services.Remove(logger);
+            builder.Services.AddScoped<ILoggingRepository, LoggingRepository>();
+
             builder.Services.AddOptions<SyncJobRepoCredentials<SyncJobRepository>>().Configure<IConfiguration>((settings, configuration) =>
             {
                 settings.ConnectionString = configuration.GetValue<string>("jobsStorageAccountConnectionString");
                 settings.TableName = configuration.GetValue<string>("jobsTableName");
             });
 
-			builder.Services.AddSingleton<IMembershipDifferenceCalculator<AzureADUser>, MembershipDifferenceCalculator<AzureADUser>>();
-			builder.Services.AddSingleton<IGraphServiceClient>((services) =>
-			{
-				return new GraphServiceClient(FunctionAppDI.CreateAuthProvider(services.GetService<IOptions<GraphCredentials>>().Value));
-			})
-			.AddScoped<IGraphGroupRepository, GraphGroupRepository>()
-			.AddScoped<ISyncJobRepository>(services =>
-			{
-				var creds = services.GetService<IOptions<SyncJobRepoCredentials<SyncJobRepository>>>();
-				return new SyncJobRepository(creds.Value.ConnectionString, creds.Value.TableName, services.GetService<ILoggingRepository>());
-			})
-			.AddScoped<SessionMessageCollector>()
-			.AddScoped<IGraphUpdater, GraphUpdaterApplication>();		
-		}
-	}
+            builder.Services.AddSingleton<IMembershipDifferenceCalculator<AzureADUser>, MembershipDifferenceCalculator<AzureADUser>>()
+            .AddSingleton<IGraphServiceClient>((services) =>
+            {
+                return new GraphServiceClient(FunctionAppDI.CreateAuthProvider(services.GetService<IOptions<GraphCredentials>>().Value));
+            })
+            .AddScoped<IGraphGroupRepository, GraphGroupRepository>()
+            .AddScoped<ISyncJobRepository>(services =>
+            {
+                var creds = services.GetService<IOptions<SyncJobRepoCredentials<SyncJobRepository>>>();
+                return new SyncJobRepository(creds.Value.ConnectionString, creds.Value.TableName, services.GetService<ILoggingRepository>());
+            })
+            .AddScoped<SessionMessageCollector>()
+            .AddScoped<IGraphUpdater, GraphUpdaterApplication>();
+        }
+    }
 }
