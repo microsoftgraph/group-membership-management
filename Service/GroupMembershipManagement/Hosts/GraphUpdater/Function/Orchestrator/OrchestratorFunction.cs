c5b035e8b790ec97d2dea64a08853777140c4211
Samyak Telsang <satelsan@microsoft.com>
2021-03-15 11:42:45 -0700

    adding starter and orchestrator

---
+++ Service/GroupMembershipManagement/Hosts/GraphUpdater/Function/Orchestrator/OrchestratorFunction.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using Entities;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Repositories.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace Hosts.GraphUpdater
+{
+    public class OrchestratorFunction
+    {
+        private readonly ILoggingRepository _loggingRepository = null;
+
+        public OrchestratorFunction(ILoggingRepository loggingRepository)
+        {
+            _loggingRepository = loggingRepository ?? throw new ArgumentNullException(nameof(loggingRepository));
+        }
+
+        [FunctionName(nameof(OrchestratorFunction))]
+        public async Task<GroupMembershipMessageResponse> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            _loggingRepository.LogMessageAsync(new LogMessage { Message = nameof(OrchestratorFunction) + " function started" }).ConfigureAwait(false);
+            var graphRequest = context.GetInput<GraphUpdaterFunctionRequest>();
+
+            var graphActivityResult = await context.CallActivityAsync<GroupMembershipMessageResponse>(nameof(GraphUpdaterFunction), graphRequest);
+
+            _loggingRepository.LogMessageAsync(new LogMessage { Message = nameof(OrchestratorFunction) + " function completed" }).ConfigureAwait(false);
+
+            return graphActivityResult;
+        }
+    }
+}
