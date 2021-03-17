f9928ddcdbc71fac0d7899e41b71c5845203a358
Grace Lovelace <glovelace@microsoft.com>
2021-03-17 14:51:19 -0700

    Add note about correctly setting reply URLs.

--- Service/GroupMembershipManagement/Hosts/Console/DemoUserSetup/Program.cs
+++ Service/GroupMembershipManagement/Hosts/Console/DemoUserSetup/Program.cs
@@ -14,12 +14,14 @@ namespace DemoUserSetup
             string clientId = AppSettings.LoadAppSettings().ClientId;
             string tenantId = AppSettings.LoadAppSettings().TenantId;
 
+            // if this doesn't work, make sure localhost is set as a reply url under Native/Desktop Client!
+            // it won't work if localhost is under web
             var publicClientApp = PublicClientApplicationBuilder.Create(clientId)
             .WithRedirectUri("http://localhost")
             .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
             .Build();
 
-            var authProvider = new InteractiveAuthenticationProvider(publicClientApp);
+			var authProvider = new InteractiveAuthenticationProvider(publicClientApp);
             var _graphServiceClient = new GraphServiceClient(authProvider);
             var users = new Users(_graphServiceClient);
             await users.addUsers();
