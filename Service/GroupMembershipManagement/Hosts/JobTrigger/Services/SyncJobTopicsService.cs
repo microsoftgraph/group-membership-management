2a29088f7b269fd992a54770a96a19a23d9e8cd1
Grace Lovelace <glovelace@microsoft.com>
2021-04-27 16:09:38 -0700

    Update job error emails.

--- Service/GroupMembershipManagement/Hosts/JobTrigger/Services/SyncJobTopicsService.cs
+++ Service/GroupMembershipManagement/Hosts/JobTrigger/Services/SyncJobTopicsService.cs
@@ -14,7 +14,8 @@ namespace Services
     {
         private const string EmailSubject = "EmailSubject";
         private const string SyncStartedEmailBody = "SyncStartedEmailBody";
-        private const string SyncDisabledEmailBody = "SyncDisabledEmailBody";
+        private const string SyncDisabledNoGroupEmailBody = "SyncDisabledNoGroupEmailBody";
+        private const string SyncDisabledNoOwnerEmailBody = "SyncDisabledNoOwnerEmailBody";
 
         private readonly ILoggingRepository _loggingRepository;
         private readonly ISyncJobRepository _syncJobRepository;
@@ -101,27 +102,13 @@ namespace Services
 
             runningJobs.ForEach(async job => await _serviceBusTopicsRepository.AddMessageAsync(job));
 
-            foreach (var failedJob in failedJobs)
-            {
-                var message = new EmailMessage
-                {
-                    Subject = EmailSubject,
-                    Content = SyncDisabledEmailBody,
-                    SenderAddress = _emailSenderAndRecipients.SenderAddress,
-                    SenderPassword = _emailSenderAndRecipients.SenderPassword,
-                    ToEmailAddresses = failedJob.Requestor,
-                    CcEmailAddresses = _emailSenderAndRecipients.SyncDisabledCCAddresses,
-                    AdditionalContentParams = new[] { failedJob.TargetOfficeGroupId.ToString() }
-                };
-                await _mailRepository.SendMailAsync(message);
-            }
         }
 
         private async Task<bool> CanWriteToGroup(SyncJob job)
         {
             foreach (var strat in new JobVerificationStrategy[] {
-                new JobVerificationStrategy { TestFunction = _graphGroupRepository.GroupExists, StatusMessage = $"Destination group {job.TargetOfficeGroupId} exists.", ErrorMessage = $"destination group {job.TargetOfficeGroupId} doesn't exist." },
-                new JobVerificationStrategy { TestFunction = (groupId) => _graphGroupRepository.IsAppIDOwnerOfGroup(_gmmAppId, groupId), StatusMessage = $"GMM is an owner of destination group {job.TargetOfficeGroupId}.", ErrorMessage = $"GMM is not an owner of destination group {job.TargetOfficeGroupId}." }})
+                new JobVerificationStrategy { TestFunction = _graphGroupRepository.GroupExists, StatusMessage = $"Destination group {job.TargetOfficeGroupId} exists.", ErrorMessage = $"destination group {job.TargetOfficeGroupId} doesn't exist.", EmailBody = SyncDisabledNoGroupEmailBody },
+                new JobVerificationStrategy { TestFunction = (groupId) => _graphGroupRepository.IsAppIDOwnerOfGroup(_gmmAppId, groupId), StatusMessage = $"GMM is an owner of destination group {job.TargetOfficeGroupId}.", ErrorMessage = $"GMM is not an owner of destination group {job.TargetOfficeGroupId}.", EmailBody = SyncDisabledNoOwnerEmailBody }})
             {
                 await _loggingRepository.LogMessageAsync(new LogMessage { RunId = job.RunId, Message = "Checking: " + strat.StatusMessage });
                 // right now, we stop after the first failed strategy, because it doesn't make sense to find that the destination group doesn't exist and then check if we own it.
@@ -129,6 +116,18 @@ namespace Services
                 if (await strat.TestFunction(job.TargetOfficeGroupId) == false)
                 {
                     await _loggingRepository.LogMessageAsync(new LogMessage { RunId = job.RunId, Message = "Marking sync job as failed because " + strat.ErrorMessage });
+					await _mailRepository.SendMailAsync(
+					new EmailMessage
+					{
+						Subject = EmailSubject,
+						Content = strat.EmailBody,
+						SenderAddress = _emailSenderAndRecipients.SenderAddress,
+						SenderPassword = _emailSenderAndRecipients.SenderPassword,
+						ToEmailAddresses = job.Requestor,
+						CcEmailAddresses = _emailSenderAndRecipients.SyncDisabledCCAddresses,
+						AdditionalContentParams = new[] { job.TargetOfficeGroupId.ToString() }
+					}
+						);
                     return false;
                 }
 
@@ -145,6 +144,7 @@ namespace Services
             public CanWriteToGroup TestFunction { get; set; }
             public string StatusMessage { get; set; }
             public string ErrorMessage { get; set; }
+            public string EmailBody { get; set; }
         }
     }
 }
