d39d5fed123dad5d923cca198bee2db19b1a1fd6
Lisa Palathingal <lipalath@microsoft.com>
2021-06-25 12:02:39 -0700

    bug fix - add missing DI

--- Service/GroupMembershipManagement/Hosts/SecurityGroup/Function/Startup.cs
+++ Service/GroupMembershipManagement/Hosts/SecurityGroup/Function/Startup.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 using Common.DependencyInjection;
+using DIConcreteTypes;
 using Hosts.FunctionBase;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +11,7 @@ using Microsoft.Graph;
 using Repositories.Contracts;
 using Repositories.GraphGroups;
 using Repositories.ServiceBusQueue;
+using Repositories.SyncJobsRepository;
 
 // see https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
 [assembly: FunctionsStartup(typeof(Hosts.SecurityGroup.Startup))]
@@ -30,15 +32,25 @@ namespace Hosts.SecurityGroup
                 settings.QueueName = configuration.GetValue<string>("membershipQueueName");
             });
 
+            builder.Services.AddOptions<SyncJobRepoCredentials<SyncJobRepository>>().Configure<IConfiguration>((settings, configuration) =>
+            {
+                settings.ConnectionString = configuration.GetValue<string>("jobsStorageAccountConnectionString");
+                settings.TableName = configuration.GetValue<string>("jobsTableName");
+            });
+
             builder.Services.AddSingleton<IGraphServiceClient>((services) =>
             {
                 return new GraphServiceClient(FunctionAppDI.CreateAuthProvider(services.GetService<IOptions<GraphCredentials>>().Value));
             })
-
             .AddSingleton<IMembershipServiceBusRepository, MembershipServiceBusRepository>((services) =>
             {
                 var config = services.GetService<IOptions<ServiceBusConfiguration>>().Value;
                 return new MembershipServiceBusRepository(serviceBusNamespacePrefix: config.Namespace, queueName: config.QueueName);
+            })
+            .AddSingleton<ISyncJobRepository>(services =>
+            {
+                var creds = services.GetService<IOptions<SyncJobRepoCredentials<SyncJobRepository>>>();
+                return new SyncJobRepository(creds.Value.ConnectionString, creds.Value.TableName, services.GetService<ILoggingRepository>());
             })
             .AddSingleton<IGraphGroupRepository, GraphGroupRepository>()
             .AddSingleton<SGMembershipCalculator>();
