069ebc3a1e55d77f94aacfe3937eb38c2ab9ef83
Grace Lovelace <glovelace@microsoft.com>
2021-08-03 17:04:56 -0700

    Fix CSV serialization.

--- Service/GroupMembershipManagement/Repositories.AzureBlobBackupRepository/AzureBlobBackupRepository.cs
+++ Service/GroupMembershipManagement/Repositories.AzureBlobBackupRepository/AzureBlobBackupRepository.cs
@@ -7,7 +7,9 @@ using Repositories.Contracts;
 using Repositories.Contracts.InjectConfig;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Repositories.AzureBlobBackupRepository
@@ -26,9 +28,13 @@ namespace Repositories.AzureBlobBackupRepository
 
 		public async Task<BackupResult> BackupEntitiesAsync(IAzureTableBackup backupSettings, List<DynamicTableEntity> entities)
 		{
+			if (!entities.Any())
+			{
+				await _loggingRepository.LogMessageAsync(new LogMessage { Message = $"Nothing to back up for {backupSettings.SourceTableName}. Skipping." });
+			}
 			BlobServiceClient blobServiceClient = new BlobServiceClient(backupSettings.DestinationConnectionString);
-			var containerName = $"{BACKUP_PREFIX}{backupSettings.SourceTableName}";
-			var blobName = $"{BACKUP_PREFIX}{backupSettings.SourceTableName}{DateTime.UtcNow.ToString(BACKUP_DATE_FORMAT)}";
+			var containerName = $"{BACKUP_PREFIX}{backupSettings.SourceTableName}".ToLowerInvariant();
+			var blobName = $"{BACKUP_PREFIX}{backupSettings.SourceTableName}{DateTime.UtcNow.ToString(BACKUP_DATE_FORMAT)}.csv";
 
 			var blobClient = blobServiceClient.GetBlobContainerClient(containerName);
 			await blobClient.CreateIfNotExistsAsync();
@@ -43,12 +49,30 @@ namespace Repositories.AzureBlobBackupRepository
 
 		private string SerializeEntities(List<DynamicTableEntity> entities)
 		{
+			var keys = entities[0].Properties.Keys;
 			using var writer = new StringWriter();
 			using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
 			{
-				csv.WriteRecords(entities);
+				csv.WriteRecords(entities.Select(TableEntityToDynamic));
 				return writer.ToString();
 			}
+		}
+
+		private dynamic TableEntityToDynamic(DynamicTableEntity entity)
+		{
+			// basically, this is needed because CSVHelper doesn't like dictionaries or DynamicTableEntries, but it does like ExpandoObjects
+			// https://joshclose.github.io/CsvHelper/examples/writing/write-dynamic-objects/
+
+			dynamic toReturn = new ExpandoObject();
+			toReturn.RowKey = entity.RowKey;
+			toReturn.PartitionKey = entity.PartitionKey;
+			toReturn.Timestamp = entity.Timestamp;
+			foreach (var kvp in entity.Properties)
+			{
+				((IDictionary<string, object>)toReturn)[kvp.Key] = kvp.Value.PropertyAsObject;
+			}
+
+			return toReturn;
 		}
 
 		public async Task DeleteBackupAsync(IAzureTableBackup backupSettings, string backupName)
