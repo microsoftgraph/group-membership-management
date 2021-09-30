cbc7d42a7695b74cadad7141bf01de12b2ebb05b
Grace Lovelace <glovelace@microsoft.com>
2021-09-29 17:03:03 -0700

    Properly log RunId when updating graph groups.

--- Service/GroupMembershipManagement/Hosts/GraphUpdater/Repositories.Integration.Tests/GraphGroupRepositoryTests.cs
+++ Service/GroupMembershipManagement/Hosts/GraphUpdater/Repositories.Integration.Tests/GraphGroupRepositoryTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 using Entities;
+using Microsoft.ApplicationInsights;
 using Microsoft.Graph;
 using Microsoft.Graph.Auth;
 using Microsoft.Identity.Client;
@@ -261,7 +262,7 @@ namespace Repositories.Integration.Tests
 
 			var authProvider = new InteractiveAuthenticationProvider(publicClientApp);
 			_graphServiceClient = new GraphServiceClient(authProvider);
-			_groupRepo = new GraphGroupRepository(_graphServiceClient, new MockLogger());
+			_groupRepo = new GraphGroupRepository(_graphServiceClient, new TelemetryClient(), new MockLogger());
 
 
 			await DeleteOldObjects("microsoft.graph.group");
