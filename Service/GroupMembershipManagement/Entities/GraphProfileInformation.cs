07e68acd5d93e5f4630bd730b86359fc7b0f85be
Daniel Luo <danielluo@microsoft.com>
2021-10-06 13:00:01 -0700

    Made GraphProfileInformation implement IEquatable so instances are comparable

--- Service/GroupMembershipManagement/Entities/GraphProfileInformation.cs
+++ Service/GroupMembershipManagement/Entities/GraphProfileInformation.cs
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Entities
 {
     [ExcludeFromCodeCoverage]
-    public class GraphProfileInformation
+    public class GraphProfileInformation: IEquatable<GraphProfileInformation>
     {
         /// <summary>
         /// Gets or sets the AAD Object Id of a Graph User
@@ -15,5 +16,15 @@ namespace Entities
         /// Gets or sets the onPremisesImmutableId of a Graph User
         /// </summary>
         public string PersonnelNumber { get; set; }
+
+        public bool Equals(GraphProfileInformation other)
+        {
+            return Id.Equals(other.Id) && PersonnelNumber.Equals(other.PersonnelNumber);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Id + PersonnelNumber).GetHashCode();
+        }
     }
 }
