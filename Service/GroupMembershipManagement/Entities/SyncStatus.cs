8fc1524b85c03f86f7510eddc8dd36a0c23ac19a
Alberto Rios <alrios@microsoft.com>
2022-04-07 11:47:29 -0700

    Renamed sync status

--- Service/GroupMembershipManagement/Entities/SyncStatus.cs
+++ Service/GroupMembershipManagement/Entities/SyncStatus.cs
@@ -9,7 +9,7 @@ namespace Entities
         Idle = 2,
         Error = 3,
         ThresholdExceeded = 4,
-        NoSourceMembership = 5,
+        CustomMembershipDataNotFound = 5,
         DestinationGroupNotFound = 6,
         NotOwnerOfDestinationGroup = 7,
         SecurityGroupNotFound = 8,
