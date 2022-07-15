ef45789b9f419818ee5cf4c856db3c83700cef74
Alberto Rios <alrios@microsoft.com>
2022-07-15 09:16:07 -0700

    updated jobscheduler mock

--- Service/GroupMembershipManagement/Hosts/JobScheduler/Services.Tests/Mocks/MockLoggingRepository.cs
+++ Service/GroupMembershipManagement/Hosts/JobScheduler/Services.Tests/Mocks/MockLoggingRepository.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 using Entities;
 using Repositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
@@ -13,8 +14,7 @@ namespace Services.Tests.Mocks
     {
         public List<LogMessage> MessagesLogged { get; set; } = new List<LogMessage>();
         public int MessagesLoggedCount => MessagesLogged.Count;
-
-        public Dictionary<string, string> SyncJobProperties { get; set; }
+        public Dictionary<Guid, LogProperties> SyncJobProperties { get; private set; } = new Dictionary<Guid, LogProperties>();
         public bool DryRun { get; set; }
 
         public Task LogMessageAsync(LogMessage logMessage, VerbosityLevel verbosityLevel = VerbosityLevel.INFO, [CallerMemberName] string caller = "", [CallerFilePath] string file = "")
@@ -27,6 +27,16 @@ namespace Services.Tests.Mocks
         {
             MessagesLogged.Add(logMessage);
             return Task.CompletedTask;
+        }
+
+        public void RemoveSyncJobProperties(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSyncJobProperties(Guid key, Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
         }
     }
 }
