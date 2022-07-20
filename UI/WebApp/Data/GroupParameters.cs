1257cd62944e29dcb5bef84ed7c742d7dcf3b2b2
Emily Chen <t-chenemily@microsoft.com>
2022-07-19 18:04:05 -0700

    implement working virtualization

---
+++ UI/WebApp/Data/GroupParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAppWithAuth.Data
+{
+    public class GroupParameters
+    {
+        private int _pageSize = 5;
+
+        public int StartIndex { get; set; }
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = value; }
+        }
+    }
+}
---
+++ UI/WebApp/Data/VirtualizeResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppWithAuth.Data
+{
+    public class VirtualizeResponse<T>
+    {
+        public List<T> Items { get; set; } 
+        public int TotalSize { get; set; }
+    }
+}
