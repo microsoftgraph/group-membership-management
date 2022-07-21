27f7163ba69c5035dfd2f0520fb8e857d5b8823b
Emily Chen <t-chenemily@microsoft.com>
2022-07-21 16:26:56 -0700

    implemented timer so that not all input changes will send requests

--- UI/ConsoleApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
--- UI/WebApp/Data/VirtualizeResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebAppWithAuth.Data
-{
-    public class VirtualizeResponse<T>
-    {
-        public List<T> Items { get; set; } 
-        public int TotalSize { get; set; }
-    }
-}
