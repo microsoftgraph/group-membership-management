d8bfe61a735660269d4ea0d37f3f1029d72e1086
Emily Chen <t-chenemily@microsoft.com>
2022-07-21 15:19:51 -0700

    working version of virtualization and querying by startswith

---
+++ UI/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
