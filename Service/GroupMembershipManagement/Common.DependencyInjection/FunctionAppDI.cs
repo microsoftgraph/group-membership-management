81aee42ef736e7fc5e11fbe35e0e4c353999088e
Grace Lovelace <glovelace@microsoft.com>
2022-08-05 12:30:03 -0700

    Remove code that interacts with the key vault to fetch the certificate.

--- Service/GroupMembershipManagement/Common.DependencyInjection/FunctionAppDI.cs
+++ Service/GroupMembershipManagement/Common.DependencyInjection/FunctionAppDI.cs
@@ -12,17 +12,6 @@ namespace Common.DependencyInjection
 {
     public static class FunctionAppDI
     {
-        public static IAuthenticationProvider CreateAuthProviderFromCertificate(GraphCredentials creds)
-        {
-            var confidentialClientApplication = ConfidentialClientApplicationBuilder
-            .Create(creds.ClientId)
-            .WithTenantId(creds.TenantId)
-            .WithCertificate(GetCertificate(creds.CertificateName, creds.KeyVaultName, creds.KeyVaultTenantId))
-            .Build();
-
-            return new ClientCredentialProvider(confidentialClientApplication);
-        }
-
         public static IAuthenticationProvider CreateAuthProviderFromSecret(GraphCredentials creds)
         {
             var confidentialClientApplication = ConfidentialClientApplicationBuilder
@@ -42,22 +31,6 @@ namespace Common.DependencyInjection
             .Build();
 
             return new UsernamePasswordProvider(publicClientApplication);
-        }
-
-        private static X509Certificate2 GetCertificate(string certificateName, string keyVaultName, string keyVaultTenantId)
-        {
-            var options = new DefaultAzureCredentialOptions();
-#if DEBUG
-            options.VisualStudioTenantId = keyVaultTenantId;
-#endif
-
-            var defaultCredential = new DefaultAzureCredential(options);
-            var keyVaultBaseUrl = new Uri($"https://{keyVaultName}.vault.azure.net/");
-            var secretClient = new SecretClient(keyVaultBaseUrl, defaultCredential);
-            var privateKey = secretClient.GetSecret(certificateName);
-            var privateKeyDecoded = Convert.FromBase64String(privateKey.Value.Value);
-            var certificate = new X509Certificate2(privateKeyDecoded, (string)null, X509KeyStorageFlags.MachineKeySet);
-            return certificate;
         }
     }
 }
