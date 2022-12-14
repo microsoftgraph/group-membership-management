8ceaef6fb16f8ab86357459fedd0465c16cf08eb
Lisa Palathingal <lipalath@microsoft.com>
2022-12-14 13:16:43 -0800

    updated function name

--- Service/GroupMembershipManagement/Hosts/AzureMaintenance/Function/Startup.cs
+++ Service/GroupMembershipManagement/Hosts/AzureMaintenance/Function/Startup.cs
@@ -69,7 +69,7 @@ namespace Hosts.AzureMaintenance
             builder.Services.AddSingleton<IKeyVaultSecret<IAzureMaintenanceService>>(services => new KeyVaultSecret<IAzureMaintenanceService>(services.GetService<IOptions<GraphCredentials>>().Value.ClientId))
             .AddSingleton<IGraphServiceClient>((services) =>
             {
-                return new GraphServiceClient(FunctionAppDI.CreateAuthProviderFromSecret(services.GetService<IOptions<GraphCredentials>>().Value));
+                return new GraphServiceClient(FunctionAppDI.CreateAuthenticationProvider(services.GetService<IOptions<GraphCredentials>>().Value));
             })
             .AddScoped<IGraphGroupRepository, GraphGroupRepository>();
 
