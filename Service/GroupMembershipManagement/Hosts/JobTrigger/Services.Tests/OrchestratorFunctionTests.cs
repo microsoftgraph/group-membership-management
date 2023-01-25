0c349a316a6ef7c4a90a825b157fd362686e9d49
Lisa Palathingal <lipalath@microsoft.com>
2023-01-24 16:08:01 -0800

    fix tests

--- Service/GroupMembershipManagement/Hosts/JobTrigger/Services.Tests/OrchestratorFunctionTests.cs
+++ Service/GroupMembershipManagement/Hosts/JobTrigger/Services.Tests/OrchestratorFunctionTests.cs
@@ -24,15 +24,23 @@ namespace Services.Tests
             var loggingRepository = new Mock<ILoggingRepository>();
             var graphRespository = new Mock<IGraphGroupRepository>();
             var jobTriggerService = new Mock<IJobTriggerService>();
+            var jobTriggerServiceInProgress = new Mock<IJobTriggerService>();
+            var jobTriggerServiceStuckInProgress = new Mock<IJobTriggerService>();
             var context = new Mock<IDurableOrchestrationContext>();
             var syncJobs = SampleDataHelper.CreateSampleSyncJobs(10, "SecurityGroup");
             var loggerJobProperties = new Dictionary<Guid, LogProperties>();
 
             loggingRepository.SetupGet(x => x.SyncJobProperties).Returns(loggerJobProperties);
             jobTriggerService.Setup(x => x.GetSyncJobsAsync(SyncStatus.Idle)).ReturnsAsync(syncJobs);
-            context.Setup(x => x.CallActivityAsync<List<SyncJob>>(It.Is<string>(x => x == nameof(SyncJobsReaderFunction)), It.IsAny<SyncStatus>()))
-                        .Returns(() => CallSyncJobsReaderFunctionAsync(loggingRepository.Object, jobTriggerService.Object));
+            jobTriggerServiceInProgress.Setup(x => x.GetSyncJobsAsync(SyncStatus.InProgress)).ReturnsAsync(new List<SyncJob>());
+            jobTriggerServiceStuckInProgress.Setup(x => x.GetSyncJobsAsync(SyncStatus.StuckInProgress)).ReturnsAsync(new List<SyncJob>());
 
+            context.Setup(x => x.CallActivityAsync<List<SyncJob>>(It.Is<string>(x => x == nameof(SyncJobsReaderFunction)), SyncStatus.Idle))
+                        .Returns(() => CallSyncJobsReaderFunctionAsync(loggingRepository.Object, jobTriggerService.Object, SyncStatus.Idle));
+            context.Setup(x => x.CallActivityAsync<List<SyncJob>>(It.Is<string>(x => x == nameof(SyncJobsReaderFunction)), SyncStatus.InProgress))
+                        .Returns(() => CallSyncJobsReaderFunctionAsync(loggingRepository.Object, jobTriggerServiceInProgress.Object, SyncStatus.InProgress));
+            context.Setup(x => x.CallActivityAsync<List<SyncJob>>(It.Is<string>(x => x == nameof(SyncJobsReaderFunction)), SyncStatus.StuckInProgress))
+                        .Returns(() => CallSyncJobsReaderFunctionAsync(loggingRepository.Object, jobTriggerServiceStuckInProgress.Object, SyncStatus.StuckInProgress));
             context.Setup(x => x.CallSubOrchestratorAsync(It.Is<string>(x => x == nameof(SubOrchestratorFunction)), It.IsAny<SyncJob>()));
 
             var orchestrator = new OrchestratorFunction(loggingRepository.Object);
@@ -43,10 +51,10 @@ namespace Services.Tests
                                 Times.Exactly(syncJobs.Count));
         }
 
-        private async Task<List<SyncJob>> CallSyncJobsReaderFunctionAsync(ILoggingRepository loggingRepository, IJobTriggerService jobTriggerService)
+        private async Task<List<SyncJob>> CallSyncJobsReaderFunctionAsync(ILoggingRepository loggingRepository, IJobTriggerService jobTriggerService, SyncStatus status)
         {
             var syncJobsReaderFunction = new SyncJobsReaderFunction(loggingRepository, jobTriggerService);
-            var jobs = await syncJobsReaderFunction.GetSyncJobsAsync(SyncStatus.Idle);
+            var jobs = await syncJobsReaderFunction.GetSyncJobsAsync(status);
             return jobs;
         }
     }
