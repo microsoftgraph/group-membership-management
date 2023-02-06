6d742f4885a864edc8170760b967b02a92b19967
abgonz <abgonz@microsoft.com>
2023-02-06 13:34:58 -0800

    undo changes to logging repository

--- Service/GroupMembershipManagement/Repositories.Contracts/ILoggingRepository.cs
+++ Service/GroupMembershipManagement/Repositories.Contracts/ILoggingRepository.cs
@@ -11,7 +11,10 @@ namespace Repositories.Contracts
 {
     public interface ILoggingRepository
     {
+        public Dictionary<Guid, LogProperties> SyncJobProperties { get; }
         public bool DryRun { get; set; }
+        public void SetSyncJobProperties(Guid key, Dictionary<string, string> properties);
+        public void RemoveSyncJobProperties(Guid key);
         Task LogMessageAsync(LogMessage logMessage, VerbosityLevel verbosityLevel = VerbosityLevel.INFO, [CallerMemberName] string caller = "", [CallerFilePath] string file = "");
         Task LogPIIMessageAsync(LogMessage logMessage, [CallerMemberName] string caller = "", [CallerFilePath] string file = "");
     }
