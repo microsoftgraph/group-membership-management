9f62d70c4b595067428a51cf2c2f6d92a3a4e9a0
Alberto Rios <alrios@microsoft.com>
2023-02-07 17:10:43 -0800

    Initial checkin

---
+++ Service/GroupMembershipManagement/GMMWebApi/GMMWebApi/Configuration/WebAPISettings.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+namespace GMMWebApi.Configuration
+{
+    public class WebAPISettings
+    {
+        public string Test { get; set; }
+        public string Sentinel { get; set; }
+    }
+}
---
+++ Service/GroupMembershipManagement/GMMWebApi/GMMWebApi/Program.cs
@@ -0,0 +1,126 @@
+using Azure.Identity;
+using GMMWebApi.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+
+namespace GMMWebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var azureADConfigSection = builder.Configuration.GetSection("AzureAd");
+            var tenantId = azureADConfigSection.GetValue<string>("TenantId");
+            var clientId = azureADConfigSection.GetValue<string>("ClientId");
+
+            builder.Services.Configure<WebAPISettings>(builder.Configuration.GetSection("WebAPI:Settings"));
+            builder.Configuration.AddAzureAppConfiguration(options =>
+            {
+                var appConfigurationEndpoint = builder.Configuration.GetValue<string>("Settings:appConfigurationEndpoint");
+                options.Connect(new Uri(appConfigurationEndpoint), new DefaultAzureCredential())
+                       .Select("WebAPI:*")
+                       .ConfigureRefresh(refreshOptions =>
+                        {
+                            refreshOptions.Register("WebAPI:Settings:Sentinel", refreshAll: true);
+                        });
+            });
+
+            // Add services to the container.
+            builder.Services.AddAzureAppConfiguration();
+
+            builder.Services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(azureADConfigSection);
+
+            builder.Services.AddApiVersioning(opt =>
+                {
+                    opt.DefaultApiVersion = new ApiVersion(1, 0);
+                    opt.AssumeDefaultVersionWhenUnspecified = true;
+                    opt.ReportApiVersions = true;
+                    opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+                });
+
+            // Add ApiExplorer to discover versions
+            builder.Services.AddVersionedApiExplorer(setup =>
+            {
+                setup.GroupNameFormat = "'v'VVV";
+                setup.SubstituteApiVersionInUrl = true;
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                // Enabled OAuth security in Swagger
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                            },
+                            new[] { "user_impersonation" }
+                        }
+                    });
+
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow()
+                        {
+                            AuthorizationUrl = new Uri($"https://login.microsoftonline.com/{tenantId}/oauth2/authorize"),
+                            TokenUrl = new Uri($"https://login.microsoftonline.com/{tenantId}/oauth2/token")
+                        }
+                    }
+                });
+            });
+
+            builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                    {
+                        var url = $"/swagger/{description.GroupName}/swagger.json";
+                        options.SwaggerEndpoint(url, description.GroupName.ToUpperInvariant());
+                        options.OAuthAppName("Swagger Client");
+                        options.OAuthClientId($"api://{clientId}");
+                        options.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+                        options.OAuthAdditionalQueryStringParams(new Dictionary<string, string> {
+                                { "scope", $"https://{clientId}/.default" },
+                                { "nonce", Guid.NewGuid().ToString() },
+                                { "resource", $"api://{clientId}" }
+                            });
+                    }
+                });
+
+            }
+
+            app.UseAzureAppConfiguration();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
