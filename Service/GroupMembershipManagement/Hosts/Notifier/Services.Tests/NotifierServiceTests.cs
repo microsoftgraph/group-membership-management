238e319c4ff311c7a7b9fe5036958769f15ca102
abgonz <abgonz@microsoft.com>
2023-02-09 16:32:57 -0800

    tests

--- Service/GroupMembershipManagement/Hosts/Notifier/Services.Tests/NotifierServiceTests.cs
+++ Service/GroupMembershipManagement/Hosts/Notifier/Services.Tests/NotifierServiceTests.cs
@@ -7,7 +7,10 @@ using Azure.Monitor.Query;
 using Azure.Monitor.Query.Models;
 using Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.Entities;
 using Moq;
+using Repositories.Contracts.AzureMaintenance;
+using Repositories.Contracts;
 using Repositories.Contracts.InjectConfig;
 using Repositories.Mail;
 using Services.Contracts;
@@ -22,115 +25,30 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Microsoft.Graph;
+
 namespace Services.Tests
 {
     [TestClass]
     public class NotifierServiceTests
     {
-        private MockLoggingRepository _mockLoggingRepository = null;
-        private Mock<LogsQueryClient> _logsQueryClient = new Mock<LogsQueryClient>();
-        private Mock<MailRepository> _mockMailRepository = null;
-        private Mock<IEmailSenderRecipient> _mailAddresses = null;
-        private NotifierService _notifierService = null;
-
-        [TestInitialize]
-        public void InitializeTest()
+        [TestMethod]
+        public async Task TestSendEmail()
         {
-            _mockLoggingRepository = new MockLoggingRepository();
-            _mockMailRepository = new Mock<MailRepository>();
-            _mailAddresses = new Mock<IEmailSenderRecipient>();
+            var loggerMock = new Mock<ILoggingRepository>();
+            loggerMock.Setup(x => x.LogMessageAsync(It.IsAny<LogMessage>(), VerbosityLevel.INFO, It.IsAny<string>(), It.IsAny<string>()));
 
-            _notifierService = new NotifierService(
-                _mockLoggingRepository,
-                _mockMailRepository.Object,
-                _mailAddresses.Object
-            );
+            var mailAddresses = new Mock<IEmailSenderRecipient>();
+            var mailRespository = new Mock<IMailRepository>();
+
+            var notifierService = new NotifierService(loggerMock.Object,
+                                                mailRespository.Object,
+                                                mailAddresses.Object
+                                                );
+
+            await notifierService.SendEmailAsync("user1@test.com;user2@test.com");
+            mailRespository.Verify(x => x.SendMailAsync(It.IsAny<EmailMessage>(), null), Times.Once());
         }
 
-        private Response<LogsQueryResult> CreateLogsQueryResult(List<(Guid Id, double Max, double Avg)> groupRuntimes)
-        {
-            var columns = new List<LogsTableColumn>();
-            var columnNames = new[] { "Destination", "MaxProcessingTime", "AvgProcessingTime" };
-            var logsTableColumnConstructor = typeof(LogsTableColumn).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic,
-                                                                                    new[] { typeof(string), typeof(LogsColumnType) });
-
-            foreach (var name in columnNames)
-            {
-                var columnType = name == "Destination" ? LogsColumnType.Guid : LogsColumnType.Real;
-                columns.Add(logsTableColumnConstructor.Invoke(new object[] { name, columnType }) as LogsTableColumn);
-            }
-
-            var rowsList = new List<string>();
-            foreach (var group in groupRuntimes)
-            {
-                rowsList.Add($"[\"{group.Id}\",{group.Max},{group.Avg}]");
-            }
-
-            var tableJSON = $"{{\"name\":\"PrimaryResult\"," +
-                            $"\"columns\":[{{\"name\":\"Destination\",\"type\":\"string\"}},{{\"name\":\"MaxProcessingTime\",\"type\":\"real\"}},{{\"name\":\"AvgProcessingTime\",\"type\":\"real\"}}]," +
-                            $"\"rows\":[{string.Join(",", rowsList)}]}}";
-
-            var jsonDocument = JsonDocument.Parse(tableJSON);
-            var jsonElementConstructor = typeof(JsonElement).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic,
-                                                                        new[] { typeof(JsonDocument), typeof(int) });
-
-            var tableJsonElement = (JsonElement)jsonElementConstructor.Invoke(new object[] { jsonDocument, 0 });
-            var logsTableConstructor = typeof(LogsTable).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic,
-                                                                        new[] { typeof(string), typeof(IEnumerable<LogsTableColumn>), typeof(JsonElement) });
-
-            JsonElement rows = default;
-            foreach (var property in tableJsonElement.EnumerateObject())
-            {
-                if (property.NameEquals("rows"))
-                {
-                    rows = property.Value.Clone();
-                    break;
-                }
-            }
-
-            var logsTable = logsTableConstructor.Invoke(new object[] { "table", columns, rows }) as LogsTable;
-            var logsQueryResultConstructor = typeof(LogsQueryResult).GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new[] { typeof(IEnumerable<LogsTable>) });
-            var logsQueryResult = logsQueryResultConstructor.Invoke(new object[] { new List<LogsTable> { logsTable } }) as LogsQueryResult;
-
-            return Response.FromValue(logsQueryResult, new TestResponse());
-        }
-    }
-    public class TestResponse : Response
-    {
-        public TestResponse()
-        {
-        }
-
-        public override int Status { get; }
-
-        public override string ReasonPhrase { get; }
-
-        public override Stream ContentStream { get; set; }
-        public override string ClientRequestId { get; set; }
-
-        public override void Dispose()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override bool ContainsHeader(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override IEnumerable<HttpHeader> EnumerateHeaders()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override bool TryGetHeader(string name, [NotNullWhen(true)] out string value)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override bool TryGetHeaderValues(string name, [NotNullWhen(true)] out IEnumerable<string> values)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
