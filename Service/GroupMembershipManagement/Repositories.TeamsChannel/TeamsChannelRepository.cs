c82ebb03cc6539c6cd7d8b66ca3501165def1b9a
Grace Lovelace <glovelace@microsoft.com>
2023-03-13 14:04:11 -0700

    Implement new teams user entity.

--- Service/GroupMembershipManagement/Repositories.TeamsChannel/TeamsChannelRepository.cs
+++ Service/GroupMembershipManagement/Repositories.TeamsChannel/TeamsChannelRepository.cs
@@ -1,10 +1,21 @@
+using Microsoft.Graph;
+using Models.Entities;
+using Repositories.Contracts;
+
 namespace Repositories.TeamsChannel
 {
-    public class TeamsChannelRepository
+    public class TeamsChannelRepository : ITeamsChannelRepository
     {
-        public TeamsChannelRepository()
+        private readonly GraphServiceClient _graphServiceClient;
+
+        public TeamsChannelRepository(GraphServiceClient graphServiceClient)
         {
-            
+            _graphServiceClient = graphServiceClient;
+        }
+
+        public async Task<IEnumerable<AzureADTeamsUser>> ReadUsersFromChannel(Guid groupId, string channelId)
+        {
+            return Enumerable.Empty<AzureADTeamsUser>();
         }
 
     }
