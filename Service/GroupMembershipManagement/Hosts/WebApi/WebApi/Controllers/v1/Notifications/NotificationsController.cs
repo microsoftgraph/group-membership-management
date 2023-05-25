963f82de74a5adff9596d61fe15573aa3c8df12a
abgonz <abgonz@microsoft.com>
2023-05-25 15:11:22 -0700

    Update NotificationsController to get the upn from the claim

--- Service/GroupMembershipManagement/Hosts/WebApi/WebApi/Controllers/v1/Notifications/NotificationsController.cs
+++ Service/GroupMembershipManagement/Hosts/WebApi/WebApi/Controllers/v1/Notifications/NotificationsController.cs
@@ -7,6 +7,7 @@ using Services.Contracts;
 using Services.Messages.Requests;
 using Services.Messages.Responses;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using WebApi.Models.Requests;
 
 namespace WebApi.Controllers.v1.Notifications
@@ -33,7 +34,7 @@ namespace WebApi.Controllers.v1.Notifications
         public async Task<ActionResult<string>> GetCardAsync(Guid id)
         {
             var request = HttpContext.Request;
-            var userUpn = getUserUpnFromRequestToken(request);
+            var userUpn = GetUserUpn();
 
             var response = await _notificationCardHandler.ExecuteAsync(new NotificationCardRequest(id, userUpn));
             Response.Headers["card-update-in-body"] = "true";
@@ -45,26 +46,17 @@ namespace WebApi.Controllers.v1.Notifications
         public async Task<ActionResult<string>> ResolveNotificationAsync(Guid id, [FromBody] ResolveNotification model)
         {
             var request = HttpContext.Request;
-            var userUpn = getUserUpnFromRequestToken(request);
+            var userUpn = GetUserUpn();
 
             var response = await _resolveNotificationHandler.ExecuteAsync(new ResolveNotificationRequest(id, userUpn, model.Resolution));
             Response.Headers["card-update-in-body"] = "true";
             return Content(response.CardJson, "application/json");
         }
 
-        private string getUserUpnFromRequestToken(HttpRequest request)
+        private string? GetUserUpn()
         {
-            var bearerToken = request.Headers["Authorization"];
-            var pureToken = bearerToken.ToString().Replace("Bearer ", string.Empty);
-
-            var jwtHandler = new JwtSecurityTokenHandler();
-            var jwtToken = jwtHandler.ReadJwtToken(pureToken);
-
-            var userUpn = jwtToken.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
-            if(userUpn == null)
-                return "";
-
-            return userUpn;
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            return identity?.Claims.Where(x => x.Type == ClaimTypes.Upn).FirstOrDefault()?.Value;
         }
     }
 }
