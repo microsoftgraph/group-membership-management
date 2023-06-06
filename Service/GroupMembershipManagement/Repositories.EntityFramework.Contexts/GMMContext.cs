f83ac8409429b73ffd830e1acfecc111adefac72
Lisa Palathingal <lipalath@microsoft.com>
2023-06-06 11:57:58 -0700

    new projects

---
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/GMMContext.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Repositories.EntityFramework.Contexts
+{
+    public class GMMContext : DbContext
+    {
+        public DbSet<SyncJob> SyncJobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SyncJob>().Property(t => t.Id)
+                .ValueGeneratedOnAdd()
+                .HasDefaultValueSql("NEWSEQUENTIALID()");
+        }
+
+        public GMMContext(DbContextOptions<GMMContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Service/GroupMembershipManagement/Repositories.EntityFramework/DatabaseSyncJobsRepository.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Repositories.Contracts;
+using Repositories.EntityFramework.Contexts;
+
+namespace Repositories.EntityFramework
+{
+    public class DatabaseSyncJobsRepository : IDatabaseSyncJobsRepository
+    {
+        private readonly GMMContext _context;
+        private readonly DbContextOptions<GMMContext> _contextOptions;
+
+        public DatabaseSyncJobsRepository(GMMContext gmmContext, DbContextOptions<GMMContext> gmmContextOptions)
+        {
+            _context = gmmContext ?? throw new ArgumentNullException(nameof(gmmContext));
+            _contextOptions = gmmContextOptions ?? throw new ArgumentNullException(nameof(gmmContextOptions));
+        }
+
+        public Task<List<SyncJob>> SelectSyncJobsAsync()
+        {
+            return _context.SyncJobs.ToListAsync();
+        }
+    }
+}
