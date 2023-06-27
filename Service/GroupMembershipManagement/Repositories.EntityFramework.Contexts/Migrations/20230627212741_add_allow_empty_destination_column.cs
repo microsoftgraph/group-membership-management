3e6a5d7dcff93a7fca030e6abe4a55a65ff1271c
Lisa Palathingal <lipalath@microsoft.com>
2023-06-27 14:34:25 -0700

    migration to add AllowEmptyDestination column

---
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20230627212741_add_allow_empty_destination_column.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.EntityFramework.Contexts.Migrations
+{
+    public partial class add_allow_empty_destination_column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowEmptyDestination",
+                table: "SyncJobs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowEmptyDestination",
+                table: "SyncJobs");
+        }
+    }
+}
