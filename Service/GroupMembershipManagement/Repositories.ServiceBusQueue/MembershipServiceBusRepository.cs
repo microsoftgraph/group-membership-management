3598e5172611000aef8d230cb81e5b294d57095b
Lisa Palathingal <lipalath@microsoft.com>
2023-06-30 14:11:35 -0700

    security group updates

--- Service/GroupMembershipManagement/Repositories.ServiceBusQueue/MembershipServiceBusRepository.cs
+++ Service/GroupMembershipManagement/Repositories.ServiceBusQueue/MembershipServiceBusRepository.cs
@@ -22,8 +22,8 @@ namespace Repositories.ServiceBusQueue
 
         public async Task SendMembership(GroupMembership groupMembership, string sentFrom = "")
         {
-            if (groupMembership.SyncJobPartitionKey == null) { throw new ArgumentNullException("SyncJobPartitionKey must be set."); }
-            if (groupMembership.SyncJobRowKey == null) { throw new ArgumentNullException("SyncJobRowKey must be set."); }
+            //if (groupMembership.SyncJobPartitionKey == null) { throw new ArgumentNullException("SyncJobPartitionKey must be set."); }
+            //if (groupMembership.SyncJobRowKey == null) { throw new ArgumentNullException("SyncJobRowKey must be set."); }
 
             foreach (var message in groupMembership.Split().Select(x => new Azure.Messaging.ServiceBus.ServiceBusMessage
             {
