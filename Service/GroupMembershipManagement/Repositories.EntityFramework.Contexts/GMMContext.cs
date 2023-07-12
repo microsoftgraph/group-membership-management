996c03311439ce26c67ffa26944a11edf71b2d31
Lisa Palathingal <lipalath@microsoft.com>
2023-07-12 13:24:10 -0700

    migration to add purged jobs table

--- Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/GMMContext.cs
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/GMMContext.cs
@@ -9,12 +9,17 @@ namespace Repositories.EntityFramework.Contexts
     public class GMMContext : DbContext
     {
         public DbSet<SyncJob> SyncJobs { get; set; }
+        public DbSet<PurgedSyncJob> PurgedSyncJobs { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SyncJob>().Property(t => t.Id)
                 .ValueGeneratedOnAdd()
                 .HasDefaultValueSql("NEWSEQUENTIALID()");
+
+            modelBuilder.Entity<PurgedSyncJob>().Property(p => p.Id)
+                  .ValueGeneratedOnAdd()
+                  .HasDefaultValueSql("NEWID()");
         }
 
         public GMMContext(DbContextOptions<GMMContext> options)
