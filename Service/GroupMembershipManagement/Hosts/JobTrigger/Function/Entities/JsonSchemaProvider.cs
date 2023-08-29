8ced54f9bd8d67d12a391208a06a601fab672894
Alberto Rios <alrios@microsoft.com>
2023-08-29 16:29:25 -0700

    Validate Query json

---
+++ Service/GroupMembershipManagement/Hosts/JobTrigger/Function/Entities/JsonSchemaProvider.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+
+namespace Hosts.JobTrigger
+{
+    public class JsonSchemaProvider
+    {
+        public Dictionary<string, string> Schemas { get; set; } = new Dictionary<string, string>();
+    }
+}
