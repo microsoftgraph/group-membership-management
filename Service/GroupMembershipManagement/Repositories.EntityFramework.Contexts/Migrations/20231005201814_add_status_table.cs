64c8cf152a3c782d80ebbc188224355a4478ef5c
Alberto Rios <alrios@microsoft.com>
2023-10-13 10:00:57 -0700

    Map ids to status

--- Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20231005201814_add_status_table.cs
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20231005201814_add_status_table.cs
@@ -36,15 +36,43 @@ namespace Repositories.EntityFramework.Contexts.Migrations
                 SyncStatus.ThresholdExceeded
             };
 
-            foreach (SyncStatus status in Enum.GetValues<SyncStatus>().OrderBy(x => x.ToString()))
+            var statuses = new Dictionary<Guid, SyncStatus>
+            {
+                { Guid.Parse("A23604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.All },
+                { Guid.Parse("A33604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.CustomerPaused },
+                { Guid.Parse("A43604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.DestinationGroupNotFound },
+                { Guid.Parse("A53604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.DestinationQueryNotValid },
+                { Guid.Parse("A63604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.DevPaused },
+                { Guid.Parse("A73604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.Error },
+                { Guid.Parse("A83604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.ErroredDueToStuckInProgress },
+                { Guid.Parse("A93604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.FileNotFound },
+                { Guid.Parse("AA3604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.FilePathNotValid },
+                { Guid.Parse("AB3604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.GuestUsersCannotBeAddedToUnifiedGroup },
+                { Guid.Parse("AC3604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.Idle },
+                { Guid.Parse("AD3604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.InProgress },
+                { Guid.Parse("AE3604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.MembershipDataNotFound },
+                { Guid.Parse("AF3604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.New },
+                { Guid.Parse("B03604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.NotOwnerOfDestinationGroup },
+                { Guid.Parse("B13604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.QueryNotValid },
+                { Guid.Parse("B23604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.SchemaError },
+                { Guid.Parse("B33604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.SecurityGroupNotFound },
+                { Guid.Parse("B43604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.StandardTeamsChannel },
+                { Guid.Parse("B53604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.StuckInProgress },
+                { Guid.Parse("B63604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.TeamsChannelError },
+                { Guid.Parse("B73604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.TeamsChannelNotDestination },
+                { Guid.Parse("B83604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.ThresholdExceeded },
+                { Guid.Parse("B93604F9-5869-EE11-9937-6045BDE913DD"), SyncStatus.TransientError }
+            };
+
+            foreach (var status in statuses)
             {
                 migrationBuilder.InsertData(
                     table: "Statuses",
-                    columns: new[] { "Name", "SortPriority" },
-                    columnTypes: new[] { "nvarchar(255)", "int" },
+                    columns: new[] { "Id", "Name", "SortPriority" },
+                    columnTypes: new[] { "uniqueidentifier", "nvarchar(255)", "int" },
                     values: new object[,]
                     {
-                        { status.ToString(), statusWithActionRequired.Contains(status)? 500 : defaultSortPriority }
+                        { status.Key, status.Value.ToString(), statusWithActionRequired.Contains(status.Value)? 500 : defaultSortPriority }
                     });
             }
 
