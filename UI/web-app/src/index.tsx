3933774eb994adea11538a7d039814ea86e1c479
Darin Hoover <dahoove@microsoft.com>
2023-12-05 13:55:46 -0800

    Add and configure Application Insights.

--- UI/web-app/src/index.tsx
+++ UI/web-app/src/index.tsx
@@ -4,6 +4,8 @@
 import { ThemeProvider, initializeIcons } from '@fluentui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -11,6 +13,29 @@ import { App } from './App';
 import { AdminConfig, JobsPage, JobDetails, OwnerPage, ManageMembership } from './pages';
 import { store } from './store';
 
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: process.env.REACT_APP_APPINSIGHTS_CONNECTIONSTRING,
+    autoTrackPageVisitTime: true,
+    enableAjaxErrorStatusText: true,
+    enableAjaxPerfTracking: true,
+    enableCorsCorrelation: true,
+    enableAutoRouteTracking: true,
+    enablePerfMgr: true,
+    namePrefix: 'GMM_AI_',
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: {
+        // history is set to null because we are using enableAutoRouteTracking.
+        // history is used to track page views that do not update the browser url.
+        // enabling both will cause app insights to report duplicate page views.
+        history: null
+      }
+    }
+  }
+});
+appInsights.loadAppInsights();
 initializeIcons();
 ReactDOM.render(
   <ThemeProvider>
