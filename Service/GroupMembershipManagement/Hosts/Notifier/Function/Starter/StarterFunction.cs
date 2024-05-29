ecfb3f5705659e48adf4d126dad1397a5ad60bc2
Alberto Rios <alrios@microsoft.com>
2024-05-29 14:07:19 -0700

    Updated Notifier to skip email notifications

--- Service/GroupMembershipManagement/Hosts/Notifier/Function/Starter/StarterFunction.cs
+++ Service/GroupMembershipManagement/Hosts/Notifier/Function/Starter/StarterFunction.cs
@@ -1,29 +1,30 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using System;
-using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Models;
 using Repositories.Contracts;
 using Repositories.Contracts.InjectConfig;
-using Azure.Messaging.ServiceBus;
-using System.Text;
-using Polly;
+using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 namespace Hosts.Notifier
 {
     public class StarterFunction
     {
+        private readonly IMailConfig _mailConfig;
         private readonly ILoggingRepository _loggingRepository = null;
         private readonly IThresholdNotificationConfig _thresholdNotificationConfig;
 
-        public StarterFunction(ILoggingRepository loggingRepository, IThresholdNotificationConfig thresholdNotificationConfig)
+        public StarterFunction(ILoggingRepository loggingRepository, IThresholdNotificationConfig thresholdNotificationConfig, IMailConfig mailConfig)
         {
             _loggingRepository = loggingRepository ?? throw new ArgumentNullException(nameof(loggingRepository));
             _thresholdNotificationConfig = thresholdNotificationConfig ?? throw new ArgumentNullException(nameof(thresholdNotificationConfig));
+            _mailConfig = mailConfig ?? throw new ArgumentNullException(nameof(mailConfig)); ;
         }
 
         [FunctionName(nameof(StarterFunction))]
@@ -46,7 +47,23 @@ namespace Hosts.Notifier
             SyncJob job = ((JsonElement)messageContent["SyncJob"]).Deserialize<SyncJob>();
             Guid runId = (Guid)job.RunId;
             _loggingRepository.SetSyncJobProperties(runId, job.ToDictionary());
-            var instanceId = await starter.StartNewAsync(nameof(OrchestratorFunction), (orchestratorRequest));
+
+            if (_mailConfig.SkipEmailNotifications)
+            {
+                await _loggingRepository.LogMessageAsync(new LogMessage
+                {
+                    RunId = runId,
+                    Message = "Email notifications are disabled."
+                });
+
+            }
+            else
+            {
+                var instanceId = await starter.StartNewAsync(nameof(OrchestratorFunction), (orchestratorRequest));
+            }
+
+
+
             await _loggingRepository.LogMessageAsync(new LogMessage { Message = $"{nameof(StarterFunction)} function completed" }, VerbosityLevel.DEBUG);
         }
     }
