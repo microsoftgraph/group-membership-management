4b6129fda44c74f93685fd54ee78dc32ecdb80f7
Daniel Luo <danielluo@microsoft.com>
2024-07-08 15:57:47 -0700

    Added Sql Migration for new NotificationType: InactiveSyncJobNotification

---
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20240708224924_notification_type_add_inactive.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.EntityFramework.Contexts.Migrations
+{
+    public partial class notification_type_add_inactive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "NotificationTypes",
+                columns: new[] { "Id", "Disabled", "Name" },
+                values: new object[] { 10, false, "InactiveSyncJobNotification" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "NotificationTypes",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
