7bd1156c66d4bcecf0117ca898ba6c31c4c77ef0
Daniel Luo <danielluo@microsoft.com>
2024-07-19 11:56:21 -0700

    Created migration for adding GuestUserFailureNotification to Sql NotificationTypes table

--- Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20240719185127_notification_type_add_guest.Designer.cs
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20240719185127_notification_type_add_guest.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repositories.EntityFramework.Contexts;
 
@@ -11,9 +12,10 @@ using Repositories.EntityFramework.Contexts;
 namespace Repositories.EntityFramework.Contexts.Migrations
 {
     [DbContext(typeof(GMMContext))]
-    partial class GMMContextModelSnapshot : ModelSnapshot
+    [Migration("20240719185127_notification_type_add_guest")]
+    partial class notification_type_add_guest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
