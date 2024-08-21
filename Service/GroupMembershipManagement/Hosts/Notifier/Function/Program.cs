7fd9d21b611142b5d9080744f6f9e9113121bcdb
yuqingyang <yuqingyang@microsoft.com>
2024-08-21 10:54:40 -0700

    Add main class

--- Service/GroupMembershipManagement/Hosts/Notifier/Function/Program.cs
+++ Service/GroupMembershipManagement/Hosts/Notifier/Function/Program.cs
@@ -18,107 +18,117 @@ using Hosts.FunctionBase;
 using Azure.Identity;
 using System.Runtime.CompilerServices;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration((context, config) =>
-     {
-        var settings = config.Build();
-        var appConfigEndpoint = GetValueOrThrow("appConfigurationEndpoint");
+namespace Hosts.JobTrigger
+{
 
-        config.AddAzureAppConfiguration(options =>
-        {
-            options.Connect(new Uri(appConfigEndpoint), new DefaultAzureCredential())
-                   .UseFeatureFlags();
-         });
-     })
-    .ConfigureServices((context, services) =>
+    public class Program
     {
-        var configuration = context.Configuration;
-        var functionName = "Notifier";
-        var dryRunSettingName = string.Empty;
-        var rootPath = context.HostingEnvironment.ContentRootPath;
-        CommonServices.ConfigureCommonServices(services, configuration, "Notifier", dryRunSettingName, rootPath);
-        services.AddOptions<HandleInactiveJobsConfig>().Configure<IConfiguration>((settings, configuration) =>
+        public static void Main (string[] args)
         {
-            settings.HandleInactiveJobsEnabled = GetBoolSetting(configuration, "AzureMaintenance:HandleInactiveJobsEnabled", false);
-            settings.NumberOfDaysBeforeDeletion = GetIntSetting(configuration, "AzureMaintenance:NumberOfDaysBeforeDeletion", 0);
-        });
-        services.AddSingleton<IHandleInactiveJobsConfig>(services =>
-        {
-            return new HandleInactiveJobsConfig(
-                services.GetService<IOptions<HandleInactiveJobsConfig>>().Value.HandleInactiveJobsEnabled,
-                services.GetService<IOptions<HandleInactiveJobsConfig>>().Value.NumberOfDaysBeforeDeletion);
-        });
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var settings = config.Build();
+                var appConfigEndpoint = GetValueOrThrow("appConfigurationEndpoint");
 
-        services.AddGraphAPIClient()
-        .AddLocalization(options =>
-        {
-            options.ResourcesPath = "Resources";
-        })
-        .AddScoped<IGraphGroupRepository, GraphGroupRepository>()
-        .AddScoped<INotifierService, NotifierService>();
+                config.AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(new Uri(appConfigEndpoint), new DefaultAzureCredential())
+                        .UseFeatureFlags();
+                });
+            })
+            .ConfigureServices((context, services) =>
+            {
+                var configuration = context.Configuration;
+                var functionName = "Notifier";
+                var dryRunSettingName = string.Empty;
+                var rootPath = context.HostingEnvironment.ContentRootPath;
+                CommonServices.ConfigureCommonServices(services, configuration, "Notifier", dryRunSettingName, rootPath);
+                services.AddOptions<HandleInactiveJobsConfig>().Configure<IConfiguration>((settings, configuration) =>
+                {
+                    settings.HandleInactiveJobsEnabled = GetBoolSetting(configuration, "AzureMaintenance:HandleInactiveJobsEnabled", false);
+                    settings.NumberOfDaysBeforeDeletion = GetIntSetting(configuration, "AzureMaintenance:NumberOfDaysBeforeDeletion", 0);
+                });
+                services.AddSingleton<IHandleInactiveJobsConfig>(services =>
+                {
+                    return new HandleInactiveJobsConfig(
+                        services.GetService<IOptions<HandleInactiveJobsConfig>>().Value.HandleInactiveJobsEnabled,
+                        services.GetService<IOptions<HandleInactiveJobsConfig>>().Value.NumberOfDaysBeforeDeletion);
+                });
 
-        services.AddOptions<ThresholdNotificationServiceConfig>().Configure<IConfiguration>((settings, configuration) =>
-        {
-            settings.ActionableEmailProviderId = configuration.GetValue<Guid>("actionableEmailProviderId");
-            settings.ApiHostname = configuration.GetValue<string>("apiHostname");
-        });
-        services.AddOptions<ThresholdConfig>().Configure<IConfiguration>((settings, configuration) =>
-        {
-            settings.MaximumNumberOfThresholdRecipients = GetIntSetting(configuration, "MaximumNumberOfThresholdRecipients", 10);
-            settings.NumberOfThresholdViolationsToNotify = GetIntSetting(configuration, "NumberOfThresholdViolationsToNotify", 3);
-            settings.NumberOfThresholdViolationsFollowUps = GetIntSetting(configuration, "NumberOfThresholdViolationsFollowUps", 3);
-            settings.NumberOfThresholdViolationsToDisableJob = GetIntSetting(configuration, "NumberOfThresholdViolationsToDisableJob", 10);
-        });
-        services.AddSingleton<IThresholdConfig>(services =>
-        {
-            return new ThresholdConfig
-                (
-                    services.GetService<IOptions<ThresholdConfig>>().Value.MaximumNumberOfThresholdRecipients,
-                    services.GetService<IOptions<ThresholdConfig>>().Value.NumberOfThresholdViolationsToNotify,
-                    services.GetService<IOptions<ThresholdConfig>>().Value.NumberOfThresholdViolationsFollowUps,
-                    services.GetService<IOptions<ThresholdConfig>>().Value.NumberOfThresholdViolationsToDisableJob
-                );
-        });
-        services.AddSingleton<IServiceBusQueueRepository, ServiceBusQueueRepository>(services =>
-        {
-            var configuration = services.GetRequiredService<IConfiguration>();
-            var failedNotificationsQueue = configuration["serviceBusFailedNotificationsQueue"];
-            var client = services.GetRequiredService<ServiceBusClient>();
-            var sender = client.CreateSender(failedNotificationsQueue);
-            return new ServiceBusQueueRepository(sender);
-        });
-        services.AddScoped<IThresholdNotificationConfig>((sp) =>
-        {
-            return new ThresholdNotificationConfig(true);
-        });
-        services.AddScoped<IThresholdNotificationService, ThresholdNotificationService>();
-        services.AddHttpClient();
+                services.AddGraphAPIClient()
+                .AddLocalization(options =>
+                {
+                    options.ResourcesPath = "Resources";
+                })
+                .AddScoped<IGraphGroupRepository, GraphGroupRepository>()
+                .AddScoped<INotifierService, NotifierService>();
 
-    })
-    .Build();
+                services.AddOptions<ThresholdNotificationServiceConfig>().Configure<IConfiguration>((settings, configuration) =>
+                {
+                    settings.ActionableEmailProviderId = configuration.GetValue<Guid>("actionableEmailProviderId");
+                    settings.ApiHostname = configuration.GetValue<string>("apiHostname");
+                });
+                services.AddOptions<ThresholdConfig>().Configure<IConfiguration>((settings, configuration) =>
+                {
+                    settings.MaximumNumberOfThresholdRecipients = GetIntSetting(configuration, "MaximumNumberOfThresholdRecipients", 10);
+                    settings.NumberOfThresholdViolationsToNotify = GetIntSetting(configuration, "NumberOfThresholdViolationsToNotify", 3);
+                    settings.NumberOfThresholdViolationsFollowUps = GetIntSetting(configuration, "NumberOfThresholdViolationsFollowUps", 3);
+                    settings.NumberOfThresholdViolationsToDisableJob = GetIntSetting(configuration, "NumberOfThresholdViolationsToDisableJob", 10);
+                });
+                services.AddSingleton<IThresholdConfig>(services =>
+                {
+                    return new ThresholdConfig
+                        (
+                            services.GetService<IOptions<ThresholdConfig>>().Value.MaximumNumberOfThresholdRecipients,
+                            services.GetService<IOptions<ThresholdConfig>>().Value.NumberOfThresholdViolationsToNotify,
+                            services.GetService<IOptions<ThresholdConfig>>().Value.NumberOfThresholdViolationsFollowUps,
+                            services.GetService<IOptions<ThresholdConfig>>().Value.NumberOfThresholdViolationsToDisableJob
+                        );
+                });
+                services.AddSingleton<IServiceBusQueueRepository, ServiceBusQueueRepository>(services =>
+                {
+                    var configuration = services.GetRequiredService<IConfiguration>();
+                    var failedNotificationsQueue = configuration["serviceBusFailedNotificationsQueue"];
+                    var client = services.GetRequiredService<ServiceBusClient>();
+                    var sender = client.CreateSender(failedNotificationsQueue);
+                    return new ServiceBusQueueRepository(sender);
+                });
+                services.AddScoped<IThresholdNotificationConfig>((sp) =>
+                {
+                    return new ThresholdNotificationConfig(true);
+                });
+                services.AddScoped<IThresholdNotificationService, ThresholdNotificationService>();
+                services.AddHttpClient();
 
-host.Run();
-static bool GetBoolSetting(IConfiguration configuration, string settingName, bool defaultValue)
-{
-    var checkParse = bool.TryParse(configuration[settingName], out bool value);
-    if (checkParse)
-        return value;
-    return defaultValue;
-}
+            })
+            .Build();
+                
+        host.Run();
+        }
+        static bool GetBoolSetting(IConfiguration configuration, string settingName, bool defaultValue)
+        {
+            var checkParse = bool.TryParse(configuration[settingName], out bool value);
+            if (checkParse)
+                return value;
+            return defaultValue;
+        }
 
-static int GetIntSetting(IConfiguration configuration, string settingName, int defaultValue)
-{
-    var checkParse = int.TryParse(configuration[settingName], out int value);
-    if (checkParse)
-        return value;
-    return defaultValue;
-}
+        static int GetIntSetting(IConfiguration configuration, string settingName, int defaultValue)
+        {
+            var checkParse = int.TryParse(configuration[settingName], out int value);
+            if (checkParse)
+                return value;
+            return defaultValue;
+        }
 
-static string GetValueOrThrow(string key, [CallerFilePath] string callerFile = "", [CallerLineNumber] int callerLine = 0)
-{
-    var value = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.Process);
-    if (string.IsNullOrWhiteSpace(value))
-        throw new ArgumentNullException($"Could not start because of missing configuration option: {key}. Requested by file {callerFile}:{callerLine}.");
-    return value;
+        static string GetValueOrThrow(string key, [CallerFilePath] string callerFile = "", [CallerLineNumber] int callerLine = 0)
+        {
+            var value = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.Process);
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException($"Could not start because of missing configuration option: {key}. Requested by file {callerFile}:{callerLine}.");
+            return value;
+        }
+    }
 }
