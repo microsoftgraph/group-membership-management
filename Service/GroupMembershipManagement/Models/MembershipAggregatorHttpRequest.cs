18b3db05232552e7a49f27158bed02b81475e3c0
Daniel Luo <danielluo@microsoft.com>
2025-07-24 08:57:46 -0700

    Split out MembershipAggregatorHttpRequest model from MembershipHttpRequest model

--- Service/GroupMembershipManagement/Models/MembershipAggregatorHttpRequest.cs
+++ Service/GroupMembershipManagement/Models/MembershipAggregatorHttpRequest.cs
@@ -3,10 +3,12 @@
 
 namespace Models
 {
-    public class MembershipAggregatorHttpRequest : MembershipHttpRequest
+    public class MembershipAggregatorHttpRequest
     {
+        public required string FilePath { get; init; }
         public required int PartNumber { get; init; }
         public required int PartsCount { get; init; }
+        public required SyncJob SyncJob { get; init; }
         public required bool IsDestinationPart { get; init; }
     }
 }
