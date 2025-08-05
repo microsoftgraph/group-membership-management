9e79774c58b4569b96961c96ec106796ba2127d8
Lisa Palathingal <lipalath@microsoft.com>
2025-08-05 14:26:25 -0700

    build prompt

--- Service/GroupMembershipManagement/Hosts/WebApi/WebApi/Controllers/v1/OpenAI/OpenAIController.cs
+++ Service/GroupMembershipManagement/Hosts/WebApi/WebApi/Controllers/v1/OpenAI/OpenAIController.cs
@@ -23,10 +23,44 @@ namespace WebApi.Controllers.v1.OpenAI
 
         [Authorize()]
         [HttpPost("generateTitle")]
-        public async Task<IActionResult> GenerateTitle([FromBody] string prompt)
+        public async Task<IActionResult> GenerateTitle([FromBody] string filter)
         {
+            var prompt = BuildTitlePrompt(filter);
             var result = await _openAIService.GetTitleAsync(prompt);
             return Ok(result);
+        }
+
+        private string BuildTitlePrompt(string filter)
+        {
+            return $@"
+
+                Create **one** string title based on the following filter.
+
+                Here is the filter: {filter}
+
+                The filter is in SQL WHERE clause format. It may contain multiple conditions combined with AND/OR.
+
+                This is the standard filter format:
+
+                [ATTRIBUTE] [OPERATOR] [VALUE] [AND/OR] [ATTRIBUTE] [OPERATOR] [VALUE] [AND/OR] [ATTRIBUTE] [OPERATOR] [VALUE] ...
+
+                For example: CountryName = 'USA' AND ChildCount > 2
+
+                ATTRIBUTE is the Column Name in SQL table
+
+                OPERATOR can be one of the following: =, <>, >, <, >=, <=, IN, NOT IN
+
+                VALUE is the value of the Column
+
+                AND/OR is the logical operator to combine multiple conditions. This can be either AND or OR. If there is only one condition, there is no AND/OR.
+
+                Create a meaningful title that summarizes the filter conditions. For example, if the filter is 'CountryName = 'USA' AND ChildCount > 2', the title could be 'All users in USA with more than 2 children'.
+
+                If the filter contains OR/AND or multiple conditions, summarize the **overall logic** in one short sentence. **Do not generate multiple titles**.
+
+                Don't include any prefixes such as ""Title:"".
+
+            ";
         }
     }
 }
