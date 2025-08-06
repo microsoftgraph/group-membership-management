1a54bcb387e5430e7115df0eb933059e57316946
Lisa Palathingal <lipalath@microsoft.com>
2025-08-06 14:00:45 -0700

    add retry logic

--- UI/web-app/src/store/title.api.tsx
+++ UI/web-app/src/store/title.api.tsx
@@ -6,30 +6,44 @@ import { config } from '../authConfig';
 import { ThunkConfig } from './store';
 import { TokenType } from '../services/auth';
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const backoff: number = 3000;
+const retries: number = 3;
+
 export const getTitle = createAsyncThunk<
   string,
   string,
   ThunkConfig
->('title', async (filter, { extra }) => {
+>('title', async (filter, { extra }): Promise<string> => {
   const { authenticationService } = extra.services;
-  const token = await authenticationService.getTokenAsync(TokenType.GMM);
-  const headers = new Headers();
-  headers.append('Authorization', `Bearer ${token}`);
-  headers.append('Content-Type', 'application/json');
 
-  const options = {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(filter),
+  const makeRequest = async (currentRetries: number, currentBackoff: number): Promise<string> => {
+    const token = await authenticationService.getTokenAsync(TokenType.GMM);
+    const headers = new Headers();
+    headers.append('Authorization', `Bearer ${token}`);
+    headers.append('Content-Type', 'application/json');
+
+    const options = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(filter),
+    };
+
+    try {
+      const response = await fetch(config.getTitle, options);
+      const responseText = await response.text();
+      return responseText;
+    }
+    catch (error: any) {
+      if (error.response && error.response.status === 429 && currentRetries > 0) {
+        console.warn(`Rate limit exceeded. Retrying in ${currentBackoff} ms...`);
+        await sleep(currentBackoff);
+        return makeRequest(currentRetries - 1, currentBackoff * 2);
+      } else {
+        throw error;
+      }
+    }
   };
-
-  try
-  {
-    const response = await fetch(config.getTitle, options);
-    const responseText = await response.text();
-    return responseText;
-  }
-  catch (error) {
-    throw new Error('Failed to generate title!');
-  }
+  return makeRequest(retries, backoff);
 });
+
