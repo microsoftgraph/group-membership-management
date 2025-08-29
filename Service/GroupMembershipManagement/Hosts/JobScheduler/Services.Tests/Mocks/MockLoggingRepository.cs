a81192ace8bdde42b08fdd416c2d68777b917095
Alberto Rios <alrios@microsoft.com>
2025-08-29 14:05:59 -0700

    Updated mock for JobScheduler

--- Service/GroupMembershipManagement/Hosts/JobScheduler/Services.Tests/Mocks/MockLoggingRepository.cs
+++ Service/GroupMembershipManagement/Hosts/JobScheduler/Services.Tests/Mocks/MockLoggingRepository.cs
@@ -38,5 +38,10 @@ namespace Services.Tests.Mocks
         {
             throw new NotImplementedException();
         }
+
+        public void UpsertSyncJobProperties(Guid key, Dictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
