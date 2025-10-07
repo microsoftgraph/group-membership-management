31d24960cf4c27254c2ffaa42f63b9b0f13dc982
Daniel Luo <danielluo@microsoft.com>
2025-10-06 17:43:04 -0700

    Updated JobTrigger to only base jobs to run on ScheduledDate, and not LastRunTime

--- Service/GroupMembershipManagement/Hosts/JobTrigger/Services/JobTriggerService.cs
+++ Service/GroupMembershipManagement/Hosts/JobTrigger/Services/JobTriggerService.cs
@@ -261,10 +261,9 @@ namespace Services
         }
         private IEnumerable<SyncJob> ApplyJobTriggerFilters(IEnumerable<SyncJob> jobs)
         {
-            var allNonDryRunSyncJobs = jobs.Where(x => ((DateTime.UtcNow - x.LastRunTime) > TimeSpan.FromHours(x.Period)) && x.IsDryRunEnabled == false && x.Status != SyncStatus.InProgress.ToString());
-            var allDryRunSyncJobs = jobs.Where(x => ((DateTime.UtcNow - x.DryRunTimeStamp) > TimeSpan.FromHours(x.Period)) && x.IsDryRunEnabled == true && x.Status != SyncStatus.InProgress.ToString());
+            var allDueToRunJobs = jobs.Where(x => !x.IsDryRunEnabled && x.Status != SyncStatus.InProgress.ToString());
             var inProgressSyncJobs = jobs.Where(x => ((DateTime.UtcNow - x.LastSuccessfulStartTime) > TimeSpan.FromHours(x.Period)) && x.Status == SyncStatus.InProgress.ToString());
-            return allNonDryRunSyncJobs.Concat(allDryRunSyncJobs).Concat(inProgressSyncJobs);
+            return allDueToRunJobs.Concat(inProgressSyncJobs);
         }
         private bool HasJobTriggerThresholdExceeded(int syncJobsCount, int totalSyncJobsCount)
         {
