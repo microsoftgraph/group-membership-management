4ae03adae359339dd8b02bde8f652f4de79fc620
Daniel Luo <danielluo@microsoft.com>
2025-10-31 11:28:45 -0700

    Added EF migration for PurgingWarning NotificationType

---
+++ Service/GroupMembershipManagement/Repositories.EntityFramework.Contexts/Migrations/20251031182226_notification_type_add_purgingwarning.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.EntityFramework.Contexts.Migrations
+{
+    /// <inheritdoc />
+    public partial class notification_type_add_purgingwarning : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "NotificationTypes",
+                columns: new[] { "Id", "Disabled", "Name" },
+                values: new object[] { 13, false, "JobPurgingWarningNotification" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "NotificationTypes",
+                keyColumn: "Id",
+                keyValue: 13);
+        }
+    }
+}
