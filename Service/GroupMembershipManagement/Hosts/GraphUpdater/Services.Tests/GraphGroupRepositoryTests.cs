0def6c12c5cf7f1f3091e959a2bb5cc6546cc490
yuqingyang <yuqingyang@microsoft.com>
2026-02-10 10:14:01 -0800

    Update graphupdater test

--- Service/GroupMembershipManagement/Hosts/GraphUpdater/Services.Tests/GraphGroupRepositoryTests.cs
+++ Service/GroupMembershipManagement/Hosts/GraphUpdater/Services.Tests/GraphGroupRepositoryTests.cs
@@ -37,7 +37,7 @@ namespace Services.Tests
             _requestAdapter.SetupProperty(x => x.BaseUrl).SetReturnsDefault(GRAPH_API_V1_BASE_URL);
             _graphRepositorySettings = new Mock<IGraphRepositorySettings>();
 
-            _graphRepositorySettings.Setup(x => x.ConcurrentWriteRequests).Returns(10);
+            _graphRepositorySettings.Setup(x => x.ConcurrentAddRequests).Returns(10);
             _graphRepositorySettings.Setup(x => x.ConcurrentRemoveRequests).Returns(10);
 
             string requestUrl = null;
