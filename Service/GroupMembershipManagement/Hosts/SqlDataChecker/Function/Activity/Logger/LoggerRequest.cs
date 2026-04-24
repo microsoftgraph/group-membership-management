8fbaf4aca7d48ad9c1f7ceb150b2d37b431e61f8
Lisa Palathingal <lipalath@microsoft.com>
2026-04-24 05:36:07 -0700

    add a new function - SqlDataChecker

---
+++ Service/GroupMembershipManagement/Hosts/SqlDataChecker/Function/Activity/Logger/LoggerRequest.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Models;
+using Repositories.Contracts;
+
+namespace SqlDataChecker
+{
+    public class LoggerRequest
+    {
+        public string Message { get; set; }
+        public SyncJob SyncJob {  get; set; }
+        public VerbosityLevel Verbosity { get; set; } = VerbosityLevel.INFO;
+    }
+}
